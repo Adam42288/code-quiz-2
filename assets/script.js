8a9ea788406055fd4350521589832e0fec2838b2
Adam Santillana <adamsantillana@AdamMacMax.local>
2022-11-12 18:10:02 -0600

    Changed all classes to ID for easier selection. made it so start button hides after you click it.

---
+++ assets/script.js
@@ -0,0 +1,60 @@
+const btn = document.getElementById('startbutton');
+
+btn.addEventListener('click',() => {
+// hide button
+btn.style.display = 'none';
+});
+
+
+var myQuestions = [
+    {
+        question: "Commonly used data types DO Not Include: ",
+        answers: {
+            a: 'strings',
+            b: 'booleans',
+            c: 'alerts',
+            d: 'numbers'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with: ",
+        answers: {
+            a: 'quotes',
+            b: 'curly brackets',
+            c: 'parenthesis',
+            d: 'square brackets'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Arrays in JavaScript can be used to store: ",
+        answers: {
+            a: 'numbers and strings',
+            b: 'other arrays',
+            c: 'booleans',
+            d: 'all of the above'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: {
+            a: 'commas',
+            b: 'curly brackets',
+            c: 'quotes',
+            d: 'parenthesis'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: {
+            a: 'JavaScript',
+            b: 'terminal/bash',
+            c: 'for loops',
+            d: 'console.log'
+        },
+        correctAnswer: 'd'
+    }
+];
