9c388e89ab9dbea1413711538161e2aec890560c
Adam Santillana <adamsantillana@AdamMacMax.local>
2022-11-13 13:25:35 -0600

    Added score component to script.js and made it so score is +10 when user gets the correct answer.

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const btn = document.getElementById('startbutton');
 const quizHeading = document.getElementById('quiz-heading');
 const startscreen = document.getElementById('startscreen');
 const timer = document.getElementById('timer');
+var score = 0;
 
 btn.addEventListener('click',() => {
 // hide button and initial screen when clicking start button.
@@ -10,92 +11,28 @@ btn.style.display = 'none';
 quizHeading.style.display = 'none';
 startscreen.style.display = 'none';
 document.getElementById("panel").style.display = "flex";
+var count = 3;
+var interval = setInterval(function(){
+    document.getElementById('timer').innerHTML='Time: ' + count;
+    count--;
+    if (count === -1){
+    clearInterval(interval);
+    document.getElementById('timer').innerHTML="All done! Your final score is " + score;
+    document.getElementById('panel').style.display = "none";
+    // or...
+    alert("You're out of time!");
+                    }
+                        }, 1000);
+                        submitButton.onclick = function() {
+                            document.getElementById('timer').innerHTML='';
+                            clearInterval(interval);
+                            document.getElementById('startscreen').innerHTML='All done! answers correct:' + numCorrect;
+                            document.getElementById('quiz').innerHTML ='';
+                        } 
 }
 )
 
-    // When user clicks on the start button, start the timer and display questions.
-    startButton.onclick = function() {
-        // adding a timer.
-        document.getElementById('startscreen').innerHTML='';
-        var count = 75;
-        generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);  
-        var interval = setInterval(function(){
-        document.getElementById('timer').innerHTML='Time: ' + count;
-        count--;
-        if (count === -1){
-        clearInterval(interval);
-        document.getElementById('timer').innerHTML="Game over, you're out of time!";
-        document.getElementById('quiz').innerHTML ='';
-        // or...
-        alert("You're out of time!");
-       // showResults(questions, quizContainer, resultsContainer);
-                        }
-                            }, 1000);
-                            submitButton.onclick = function() {
-                                document.getElementById('timer').innerHTML='';
-                                clearInterval(interval);
-                                document.getElementById('startscreen').innerHTML='All done! answers correct:' + numCorrect;
-                                document.getElementById('quiz').innerHTML ='';
-                            }       
-                            }
-                            
 
-
-
-
-
-// var myQuestions = [
-//     {
-//         question: "Commonly used data types DO Not Include: ",
-//         answers: {
-//             a: 'strings',
-//             b: 'booleans',
-//             c: 'alerts',
-//             d: 'numbers'
-//         },
-//         correctAnswer: 'c'
-//     },
-//     {
-//         question: "The condition in an if / else statement is enclosed with: ",
-//         answers: {
-//             a: 'quotes',
-//             b: 'curly brackets',
-//             c: 'parenthesis',
-//             d: 'square brackets'
-//         },
-//         correctAnswer: 'c'
-//     },
-//     {
-//         question: "Arrays in JavaScript can be used to store: ",
-//         answers: {
-//             a: 'numbers and strings',
-//             b: 'other arrays',
-//             c: 'booleans',
-//             d: 'all of the above'
-//         },
-//         correctAnswer: 'd'
-//     },
-//     {
-//         question: "String values must be enclosed within ____ when being assigned to variables.",
-//         answers: {
-//             a: 'commas',
-//             b: 'curly brackets',
-//             c: 'quotes',
-//             d: 'parenthesis'
-//         },
-//         correctAnswer: 'c'
-//     },
-//     {
-//         question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-//         answers: {
-//             a: 'JavaScript',
-//             b: 'terminal/bash',
-//             c: 'for loops',
-//             d: 'console.log'
-//         },
-//         correctAnswer: 'd'
-//     }
-// ];
 
 // Questions will be asked
 
@@ -231,6 +168,7 @@ evaluate[0].addEventListener("click", () => {
     if (selected == "true") {
         result[0].innerHTML = "True";
         result[0].style.color = "green";
+        score = score +10;
     } else {
         result[0].innerHTML = "False";
         result[0].style.color = "red";
